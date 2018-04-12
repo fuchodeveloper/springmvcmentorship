2774c06031e6ebbba0b96d01bb14ec7aa79aa031
fuchodeveloper <fredrick.mgbeoma@andela.com>
2018-04-12 10:33:38 +0100

    return array or fruits

--- src/main/java/com/andela/AppController.java
+++ src/main/java/com/andela/AppController.java
@@ -1,12 +1,27 @@
 package com.andela;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@Controller
 public class AppController {
 	@RequestMapping("/")
 	public String index() {
-		return "This is a sample text from AppController using RequestMapping";
+		return "welcome";
 	}
+
+	@RequestMapping("/fruits")
+	public List<Fruits> getAllFruits() {
+		return Arrays.asList(
+				new Fruits("one", "Orange", "oval shape", "five"),
+				new Fruits("two", "Pear", "pear shape", "twenty"),
+				new Fruits("three", "Apple", "apple shape", "ten")
+		);
+	}
+
 }
---
+++ src/main/java/com/andela/Fruits.java
@@ -0,0 +1,52 @@
+package com.andela;
+
+public class Fruits {
+
+	private String id;
+	private String name;
+	private String shape;
+	private String quantity;
+
+
+	public Fruits(String id, String name, String shape, String quantity) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.shape = shape;
+		this.quantity = quantity;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getShape() {
+		return shape;
+	}
+
+	public void setShape(String shape) {
+		this.shape = shape;
+	}
+
+	public String getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+
+
+}
