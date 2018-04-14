7951ae76f48024cb739b6c79009c1a54f72bc138
fuchodeveloper <fredrick.mgbeoma@andela.com>
2018-04-14 01:52:38 +0100

    display fruits on frontend

--- src/main/java/com/andela/AppController.java
+++ src/main/java/com/andela/AppController.java
@@ -4,15 +4,17 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-//@Controller
+//@RestController
+@Controller
 public class AppController {
 	@RequestMapping("/")
-	public String index() {
-		return "welcome";
+	public String index(Model model) {
+		model.addAttribute("fruits", getAllFruits());
+		return "fruits";
 	}
 
 	@RequestMapping("/fruits")
